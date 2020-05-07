c268d7f07a09127cd2241f9ff277f6be80e23c1e
Tao Hansen <tao.santos@somosicev.com>
2020-05-06 23:33:52 -0300

    Falta Quartil

--- Estatistica-Data/Dataset-Vinho/main.py
+++ Estatistica-Data/Dataset-Vinho/main.py
@@ -12,13 +12,23 @@ import pandas as pd
 
 wine = pd.read_csv("wineDataSet.csv", skiprows=0, header=None)
 
-wine.columns = ['class', 'alcohol', 'malicAcid', 'ash', 'ashalcalinity'
+wine.columns = ['classOfWine', 'alcohol', 'malicAcid', 'ash', 'ashalcalinity'
     , 'magnesium', 'totalPhenols', 'flavanoids', 'nonFlavanoidPhenols', 'proanthocyanins'
     , 'colorIntensity', 'hue', 'od280_od315', 'proline']
 
-
 print("### Média ###")
 print(wine.mean(axis=0))
-
-#print(wine['alcohol'].mean())
-#print(wine['malicAcid'].mean())
+print("\n### Mediana ###")
+print(wine.median(axis=0))
+print("\n### Desvio Padrão ###")
+print(wine.std(axis=0))
+print("\n### Valor Máximo ###")
+print(wine.max(axis=0))
+print("\n### Valor Mínimo ###")
+print(wine.min(axis=0))
+print("\n### Variância ###")
+print(wine.var(axis=0))
+print("\n### Variância ###")
+print(wine.var(axis=0))
+print("\n### QUARTIL 1 ###")
+print(wine.quantile(.1, axis = 0))
